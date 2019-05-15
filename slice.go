4eb1bcf9724cf00bf7e538cbb1aa3ebfccd1641e
abejide001 <abejidefemi1@gmail.com>
2019-05-15 20:09:34 +0100

    implement map and restructure code

--- slice.go
+++ slice.go
@@ -8,30 +8,19 @@ func main() {
 	slice()
 	mapping()
 }
+// using slice
 func slice() {
-	// y := []string{"money", "cash", "adsd", "dsfsf", "fdgggg"}
-	// x := []string{"money", "cash", "adsd", "dsfsf", "fdgggg", "ghost"}
-	// rem := [][]string{x, y}
-	// fmt.Println(rem)
-	x := []int{1,3,24,5,6,76}
-	y := append(x[1:3])
-	fmt.Println(y)
+	y := []string{"money", "cash", "adsd", "dsfsf", "fdgggg"}
+	x := []string{"money", "cash", "adsd", "dsfsf", "fdgggg", "ghost"}
+	rem := [][]string{x, y}
+	fmt.Println(rem)
 }
-type Person struct {
-	first string
-	last string
-}
-
+// mapping
 func mapping() {
-	// x := map[string]int{   
-	// 	"John": 90,
-	// }
-	// x["Mumum"] = 20
-	// delete(x, "Mumum")
-	// fmt.Println(x)
-	p1 := Person{
-		first: "Boss",
-		last: "fsfdg",
+	x := map[string]int{   
+		"John": 90,
 	}
-   fmt.Println(p1.first)
+	x["Mumum"] = 20
+	delete(x, "Mumum")
+	fmt.Println(x)
 }
