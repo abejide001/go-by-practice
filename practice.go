2537afc223ae3dd53a4987d1dc591ab399ec019b
abejide001 <abejidefemi1@gmail.com>
2019-05-15 20:37:29 +0100

    implement struct and refactor code

--- practice.go
+++ practice.go
@@ -7,7 +7,9 @@ import (
 func main() {
 	slice()
 	mapping()
+	obj()
 }
+
 // using slice
 func slice() {
 	y := []string{"money", "cash", "adsd", "dsfsf", "fdgggg"}
@@ -15,12 +17,27 @@ func slice() {
 	rem := [][]string{x, y}
 	fmt.Println(rem)
 }
+
 // mapping
 func mapping() {
-	x := map[string]int{   
+	x := map[string]int{
 		"John": 90,
 	}
 	x["Mumum"] = 20
 	delete(x, "Mumum")
 	fmt.Println(x)
+}
+
+// Person struct
+type Person struct {
+	firstName string
+	lastName  string
+}
+
+func obj() {
+	fullName := Person{
+		firstName: "john",
+		lastName:  "aliu",
+	}
+	fmt.Println("full name is", fullName)
 }
